d240e3e9d1535c8dce153ee2d6935a9306c7d8b9
Kelly <kellysue3@yahoo.com>
2024-10-26 11:20:30 -0600

    Updates

--- WildFiresAnalyses/WildFiresAnalyses.py
+++ WildFiresAnalyses/WildFiresAnalyses.py
@@ -4,31 +4,7 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
-#conn = sqlite3.connect(r'C:\Users\Kelly\Desktop\WildFires/WildFires.db')
-
-
-
-#query = 'SELECT NWCG_GENERAL_CAUSE, COUNT(*) AS cause_count FROM Fires GROUP BY NWCG_GENERAL_CAUSE'
-
-#df = pd.read_sql_query(query, conn)
-
-#conn.close()
-
-##print(df.head())
-
-#plt.figure(figsize=(6,4))
-#plt.bar(df['NWCG_GENERAL_CAUSE'], df['cause_count'], color = 'skyblue')
-#plt.xlabel('Fire Cause')
-#plt.ylabel("Number of Fires")
-#plt.title('Number of Fires by Cause')
-#plt.xticks(rotation=45, ha='right')
-
-#plt.tight_layout()
-#plt.show()
-
-
-conn = sqlite3.connect(r'C:\Users\Kelly\Desktop\WildFires\WildFires.db')
-
+conn = sqlite3.connect(r'.\WildFires.db')
 
 # Query the data
 query =  'SELECT NWCG_GENERAL_CAUSE, COUNT(*) AS cause_count FROM Fires GROUP BY NWCG_GENERAL_CAUSE'
@@ -36,8 +12,15 @@ query =  'SELECT NWCG_GENERAL_CAUSE, COUNT(*) AS cause_count FROM Fires GROUP BY
 data = pd.read_sql_query(query, conn)
 
 # Export to CSV
-#data.to_csv('C:\Users\Kelly\Desktop\WildFires\output_data.csv', index=False)  # Save as CSV
-data.to_csv('C:/Users/Kelly/Desktop/WildFires/output_data.csv', index=False)  # Save as CSV
-# Or export to Excel
-#data.to_excel('output_data.xlsx', index=False)  # Save as Excel
+data.to_csv('./output_data.csv', index=False)  # Save as CSV
 
+#Fire count by state
+queryState = 'SELECT STATE, COUNT(*) AS fire_count FROM Fires GROUP BY STATE'
+dataState = pd.read_sql_query(queryState, conn)
+dataState_sorted = dataState.sort_values(by = 'fire_count', ascending = False)
+#Print to Console
+for index, row in dataState_sorted.iterrows():
+    print(f"State: {row['STATE']}, Number of Fires: {row['fire_count']}")
+
+#Export to CSV
+#dataState.to_csv('./state_count', index=False)
